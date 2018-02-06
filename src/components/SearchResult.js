dec5449474977952011f951426fb5e9d884ca8f3
Pek1s <riku.pekis@gmail.com>
2018-02-06 15:46:45 +0200

    fix error if no picture

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -13,25 +13,24 @@ export default class SearchResult extends React.Component {
       <FlatList
         data={ store.getState().artists.items }
         renderItem={({item}) =>
-        <View>
-          <Image
-            style={{width: 200, height: 200}}
-            source={{uri: item.images[0].url}}/>
-          <Text>{item.name}</Text>
-        </View>
+          <View>
+            <Image
+              style={{width: 200, height: 200}}
+              source={item.images.length > 0  && {uri: item.images[0].url}}/>
+            <Text>{item.name}</Text>
+          </View>
         }
-
         keyExtractor={item => item.id}
       />
       <FlatList
         data={ store.getState().albums.items }
         renderItem={({item}) =>
-        <View>
-          <Image
-            style={{width: 200, height: 200}}
-            source={{uri: item.images[0].url}}/>
-          <Text>{item.name}</Text>
-        </View>
+          <View>
+            <Image
+              style={{width: 200, height: 200}}
+              source={item.images.length > 0  && {uri: item.images[0].url}}/>
+            <Text>{item.name}</Text>
+          </View>
         }
         keyExtractor={item => item.id}
       />
