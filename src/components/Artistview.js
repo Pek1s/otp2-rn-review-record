cdb1344a188ad45eb5c75711fa151bbcf525a499
ancalego <joni.adamsson@gmail.com>
2018-03-12 04:28:12 +0200

    fixed images still no albums.

--- src/components/Artistview.js
+++ src/components/Artistview.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { FlatList, StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Dimensions } from 'react-native';
 import { store } from '../Store.js';
-import Albumresult from './Albumresult';
+import AlbumView from  '../components/Albumview';
+import { getSeveralAlbums } from '../Spotify';
 const notFoundImage = require('../images/question-mark.jpg');
 
 
@@ -13,30 +14,70 @@ export default class ArtistView extends React.Component {
   render() {
 
     return (
+      <View style={styles.container}>
+          <View style={styles.top}>
+            <Text style={styles.resultText}>{ this.props.artistName }</Text>
+            <View style={styles.imageWrap}>
+              <Image style={styles.images} source={{uri: this.props.artistImg }}/>
+            </View>
+            <Text style={styles.resultText}>{ this.props.spotifyid }</Text>
+          </View>
+      </View>
+
+
+/*
           <View style={styles.wat}>
           <Albumresult/>
           <Text style={styles.resultText}>{ this.props.spotifyid }</Text>
           <Text style={styles.resultText}>{ this.props.artistName }</Text>
            <Text style={styles.resultText}>{ this.props.artistImg }</Text>
+*/
 
-          </View>
         );
       }
     };
 
     const styles = StyleSheet.create({
-      wat: {
+      container: {
         flex: 1,
+      },
+      top: {
+        flex: 2,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        backgroundColor: '#242628',
+      },
+      imageWrap:{
+        height: (Dimensions.get('window').height/3) - 12,
+        width: (Dimensions.get('window').width/2) - 4,
+        borderWidth: 4,
+        marginBottom: 10,
+        borderColor: '#fff'
       },
 
       resultText: {
         textAlign: 'center',
         fontWeight: '500',
+        fontSize: 22,
         color: 'rgba(255, 255, 255, 0.7)',
       },
       images: {
-        borderRadius: 120,
         height: '100%',
-        width: '100%',
-         },
+        width: '100%'
+      },
+      Button: {
+        width: '80%',
+        height: '15%',
+        width: 120,
+        margin: 5,
+        paddingTop: 5,
+        backgroundColor: '#35912e',
+        borderRadius: 25,
+      },
+      buttonText: {
+        fontSize: 20,
+        fontWeight: '500',
+        color: 'rgba(255, 255, 255, 0.7)',
+        textAlign: 'center',
+      },
     });
