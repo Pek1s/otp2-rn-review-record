fb3bd3e48cd30d06f25858e73b94210dcc3718f2
ancalego <joni.adamsson@gmail.com>
2018-04-24 21:35:06 +0300

    added rating fields missing data to and from db

--- src/components/Albumview.js
+++ src/components/Albumview.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, Dimensions, TouchableOpacity } from 'react-native';
+import axios from 'axios'
+import { StyleSheet, Text, TextInput, View, Image, Dimensions, TouchableOpacity } from 'react-native';
 import { store } from '../Store.js';
 import { Actions } from 'react-native-router-flux';
 import Reviews from '../components/Reviews.js'
@@ -9,8 +10,40 @@ const notFoundImage = require('../images/question-mark.jpg');
 export default class AlbumView extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { rating: '' };
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
+  onSubmit(){
+    Keyboard.dismiss()
+/*
+axios.post(
+    'http://review-a-record.herokuapp.com/test-token',
+    {token: this.props.jwttoken}
+).then((res) => {
+    axios.post('',{
+        user_id: res.data.userid,
+        artist_name: this.props.artistname,
+        album_name: this.props.albumName,
+        spotify_artist_id: this.props.artistid,
+        spotify_album_id: this.props.spotifyid,
+        rating: this.props.rating
+      },
+      {headers: {token: this.props.jwttoken}}
+      )
+      .then((resp) => {
+          console.log(this.props.albumImg)
+        Actions.album({
+            spotifyid: this.props.spotifyid,
+            artistname: this.props.artistname,
+            artistid: this.props.artistid,
+            albumName: this.props.albumName,
+            albumImg: this.props.albumImg
+        })
+      })
+})
+*/
+  }
   render() {
 
     return (
@@ -19,6 +52,13 @@ export default class AlbumView extends React.Component {
             <View style={styles.imageWrap}>
               <Image style={styles.images} source={{uri: this.props.albumImg }}/>
             </View>
+            <Text style={styles.resultText}>{ "AVG(RATING) from db"}</Text>
+            <TextInput style={styles.inputBox}
+            placeholder = "1-5"
+            keyboardType = 'numeric'
+              onChangeText={(rating) => this.setState({rating})}
+              value={this.state.rating}
+            />
             <Text style={styles.resultText}>{ this.props.albumName }</Text>
             <TouchableOpacity style={styles.Button} onPress={() => Actions.review({
               spotifyid: this.props.spotifyid,
@@ -79,6 +119,17 @@ export default class AlbumView extends React.Component {
         textAlign: 'center',
         fontWeight: '500',
         fontSize: 22,
+        color: 'rgba(255, 255, 255, 0.7)',
+      },
+      inputBox: {
+        backgroundColor: '#3f423f',
+        width: '75%',
+        height: 40,
+        margin: 5,
+        paddingHorizontal: 30,
+        fontSize: 20,
+        fontWeight: 'bold',
+        borderRadius: 25,
         color: 'rgba(255, 255, 255, 0.7)',
       },
     });
