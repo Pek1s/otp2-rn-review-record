ffb36411eda3aeaafd83a7211572041d7edbbf24
hanhav <33927775+hanhav@users.noreply.github.com>
2018-04-26 10:46:30 +0300

    add styling to language settings

--- src/pages/LanguageSettings.js
+++ src/pages/LanguageSettings.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { View, StyleSheet, Text, ScrollView, TouchableOpacity, Platform} from 'react-native';
+import { View, StyleSheet, Text, ScrollView, TouchableOpacity, Platform, Dimensions} from 'react-native';
 import I18n from '../utils/i18n';
+import { Actions } from 'react-native-router-flux';
 
 const language = [
     {lang: "English", code: "en"},
     {lang: "Swedish", code: "sw"},
 ]
-
 
 export default class LanguageSettings extends React.Component {
 
@@ -15,18 +15,19 @@ export default class LanguageSettings extends React.Component {
         this.state = {
             languages: [],
             value: true,
-            langValue:"en",
         }
-        this.onLanguage=this.onLanguage.bind(this);
     }
+
     onSelectLanguage() {
         return(
-            language.map((data, i)=>{
-                return(
-                    <View key={i} >
-                    <TouchableOpacity onPress={()=>this.onSelectedLanguage(data)}>
-                    <Text>{data.lang}</Text>
-                    </TouchableOpacity>
+            language.map((data, i)=> {
+                return (
+                    <View key={i} style={styles.tabsItem}>
+                        <View key={i} style={styles.tabsInnerItem}>
+                            <TouchableOpacity onPress={()=>this.onSelectedLanguage(data)}>
+                                <Text style={styles.generalText}>{data.lang}</Text>
+                            </TouchableOpacity>
+                        </View>
                     </View>
                 )
             })
@@ -35,31 +36,19 @@ export default class LanguageSettings extends React.Component {
 
     onSelectedLanguage(text) {
         this.setState({
-            value: false,
             select: text.lang,
-        }),
-        I18n.locale = text.code;
-    }
-    onLanguage() {
-        this.setState({
-            value: true,
         })
+        I18n.locale = text.code;
+        Actions.refresh('language');
     }
 
     render() {
       return (
         <View style={styles.container}>
             <Text style={styles.biggerGeneralText}>{I18n.t('langSettings.SelectLang')}</Text>
-        <View>
-            <TouchableOpacity onPress={this.onLanguage}>
             <View>
-                <Text style={styles.generalText}>{this.state.select}</Text>
+                {(this.state.value) ? this.onSelectLanguage() : null}
             </View>
-        </TouchableOpacity>
-        <View>
-            {(this.state.value) ? this.onSelectLanguage() : null}
-        </View>
-        </View>
         </View>
       )
     }
@@ -70,8 +59,21 @@ export default class LanguageSettings extends React.Component {
       flex: 1,
       backgroundColor: '#242628',
       justifyContent: 'center',
-      alignItems: 'center',
+      
     },
+    tabsItem: {
+        height: Dimensions.get("window").height / 8,
+        width: Dimensions.get("window").width - 12,
+        padding: 4,
+        borderBottomWidth : 2,
+        borderBottomColor: '#2f3235',
+      },
+      tabsInnerItem: {
+        height: "100%",
+        width: "100%",
+        backgroundColor: "transparent",
+        justifyContent: "center"
+      },
     biggerGeneralText: {
         color: "rgba(255, 255, 255, 0.6)",
         fontSize: 22,
